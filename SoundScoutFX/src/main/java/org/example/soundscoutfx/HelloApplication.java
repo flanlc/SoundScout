465f7127ed55a6c141031cc11611f4f48ea0404b
LethalChief <157604660+LethalChief@users.noreply.github.com>
2024-10-03 23:05:13 -0400

    corrected a few things in fxml file

--- SoundScoutFX/src/main/java/org/example/soundscoutfx/HelloApplication.java
+++ SoundScoutFX/src/main/java/org/example/soundscoutfx/HelloApplication.java
@@ -15,7 +15,7 @@ import java.util.ArrayList;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/org/example/soundscoutfx/hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
